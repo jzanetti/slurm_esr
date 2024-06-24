3be0636f93f0822ae4c3b3cf5a1a106a824bf582
jzanetti <zsjzyhzp@gmail.com>
2024-06-25 10:26:44 +1200

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="slurm_esr",
-    version="0.0.1",
+    version="0.0.2",
     setup_requires=["setuptools-git-versioning"],
     packages=find_packages(),
     install_requires=[],
